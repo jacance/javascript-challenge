216d9b48d078ae4c142afbb57650055a74bafaf3
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-01-26 00:58:34 -0800

    table working

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,56 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#filters");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Use form input to filter the data by date
+    var filterData = tableData.filter(x => x.datetime === inputValue);
+  
+    // console.log(inputValue);
+    // console.log(filterData);
+    
+    // var summary = d3.select(".table-head")
+    var table = d3.select("#ufo-table")
+
+
+    filterData.forEach(function(tabledata) {
+        var tr = table.append("tr");
+
+        tr.append("td").text(tabledata.datetime)
+        tr.append("td").text(tabledata.city)
+        tr.append("td").text(tabledata.state)
+        tr.append("td").text(tabledata.country)
+        tr.append("td").text(tabledata.shape)
+        tr.append("td").text(tabledata.durationMinutes)
+        tr.append("td").text(tabledata.comments)
+
+    })
+
+    // Object.entries(filterData).forEach(([key, value])=>{
+    //     tr = table.append("tr");
+    //     tr.append("td").text(key);
+    //     tr.append("td").text(value);
+    //     console.log(key, value);
+    // })
+
+  };
